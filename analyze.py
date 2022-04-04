ba2457cbb2f727491a25421c3caf213cbc9a0e96
오현석 <ithing@ncsoft.com>
2022-04-04 20:12:16 +0900

    RawEvent -> BundleEvent -> FinalEvent Slot: MultiTouch dd

--- analyze.py
+++ analyze.py
@@ -1,14 +1,16 @@
 # from typing import List
 # import
 
-class MyEvent:
+class Slot:
     def __init__(self):
-        self.down_time = None
-        self.up_time = None
         self.x_start = None
-        self.x_end = None
         self.y_start = None
+
+        self.x_end = None
         self.y_end = None
+
+        self.x_diff = None
+        self.y_diff = None
 
     def set_x(self, x):
         if self.x_start is None:
@@ -22,16 +24,64 @@ class MyEvent:
         else:
             self.y_end = y
 
+    def finalize(self):
+        # print("Slot::finalize")
+        # print(f"{self.x_start}, {self.y_start}, {self.x_end}, {self.y_end}")
+
+        self.x_diff = self.x_start - self.x_end
+        self.y_diff = self.y_start - self.y_end
+        # is_swipe = True if self.y_diff > 50 else False
+
+
+class FinalEvent:
+    def __init__(self):
+        self.down_time = None
+        self.up_time = None
+        self.slots = []
+
     def set_down(self):
         pass
 
     def set_up(self):
         pass
 
+    def set_slot(self, slotid, x, y):
+        while len(self.slots) <= slotid:
+            self.slots.append(Slot())
+
+        self.slots[slotid].set_x(x)
+        self.slots[slotid].set_y(y)
+
     def finalize(self):
-        self.x_diff = self.x_start - self.x_end
-        self.y_diff = self.y_start - self.y_end
-        is_swipe = True if self.y_diff > 50 else False
+        for slot in self.slots:
+            slot.finalize()
+
+
+class BundleEvent:
+    def __init__(self):
+        self.up = False
+        self.slotid = 0
+        self.x = None
+        self.y = None
+
+    def set_up(self):
+        self.up = True
+
+    def is_up(self):
+        return self.up
+
+    def set_slotid(self, id):
+        self.slotid = id
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def finalize(self, fe: FinalEvent):
+        if self.x is not None and self.y is not None:
+            fe.set_slot(self.slotid, self.x, self.y)
 
 
 class RawEvent:
@@ -42,25 +92,28 @@ class RawEvent:
         self.name = c
         self.value = d
         self.value_int = int(d, 16)
-        self.is_up = False
         # self.result = down
 
-    def analyze(self, m: MyEvent):
+    def analyze_to_bundle(self, be: BundleEvent):
         if self.type == '0001' and self.name == '014a':  # EV_KEY, BTN_TOUCH
             if self.value == '00000001':
-                m.set_down()
+                # be.set_down()
+                pass
             elif self.value == '00000000':
-                self.is_up = True
-                m.set_up()
-
+                # self.is_up = True
+                be.set_up()
         elif self.type == '0003':  # EV_ABS
             if self.name == '0035':  # ABS_MT_POSITION_X
-                m.set_x(self.value_int)
+                be.set_x(self.value_int)
             elif self.name == '0036':  # ABS_MT_POSITION_Y
-                m.set_y(self.value_int)
+                be.set_y(self.value_int)
 
-    def isUp(self):
-        return self.is_up
+            # elif self.name == '0039':  # ABS_MT_TRACKING_ID
+            # if self.value == 'ffffffff' // 4294967295
+            #   this is end of TRACKING
+
+            elif self.name == '002f':  # ABS_MT_SLOT
+                be.set_slotid(self.value_int)
 
     def isSyn(self):
         return (self.type == '0000')  # EV_SYN, SYN_REPORT, 00000000
@@ -68,51 +121,65 @@ class RawEvent:
 
 class Worker:
     def __init__(self):
-        self.cnt_process = 0
-        self.m_list = []
+        self.eof = False
 
-    def process(self, line: str, m: MyEvent):
-        e: RawEvent = RawEvent(line)
-        e.analyze(m)
+        self.new_bundle: bool = True
+        self.be: BundleEvent = None
 
-        if e.isUp():
-            self.cnt_process += 1
-            print(f'end of event {self.cnt_process}')
-            m.finalize()
-            self.m_list.append(m)
-            return True
+        self.new_final: bool = True
+        self.fe: FinalEvent = None
+        self.cnt_final = 0
+        self.final_list = []
 
-        return False
+    def bundle_to_final(self):
+        if self.new_final:
+            self.fe = FinalEvent()
+            self.new_final = False
+
+        self.be.finalize(self.fe)
+
+        if self.be.is_up():
+            self.cnt_final += 1
+            print(f'New Final Event {self.cnt_final}')
+
+            self.fe.finalize()
+            self.final_list.append(self.fe)
+
+            self.new_final = True
+            self.fe = None
 
     def main(self):
         with open('record.txt') as f:
-            started = False
-            while (True):
+            while True:
                 line = f.readline()
                 if not line:
-                    print('ERROR: No lines...')
-                    return
-
-                if line.startswith("/dev"):
-                    started = True
+                    # print("End Of File")
                     break
 
-            if not started:
-                return
+                if not line.startswith("/dev"):
+                    continue
 
-            m = MyEvent()
-            while (True):
-                is_end = self.process(line, m)
+                if self.new_bundle:
+                    self.be = BundleEvent()
+                    self.new_bundle = False
 
-                line = f.readline()
-                if not line:
-                    break
-                elif is_end:
-                    m = MyEvent()
+                re = RawEvent(line)
+                re.analyze_to_bundle(self.be)
 
-        for i, m in enumerate(self.m_list):
-            print(f"{i}: {m.x_start}, {m.y_start} -> {m.x_end}, {m.y_end} "
-                  f"({m.x_diff}, {m.y_diff})")
+                if re.isSyn():
+                    self.bundle_to_final()
+
+                    self.new_bundle = True
+                    self.be = None
+
+        # 최종 체크
+        for i, f in enumerate(self.final_list):
+            print(f"{i}: ...")
+            for j, s in enumerate(f.slots):
+                msg = f"Slot {j} : "
+                msg += f"{s.x_start}, {s.y_start} -> {s.x_end}, {s.y_end} "
+                msg += f"({s.x_diff}, {s.y_diff})"
+                print(msg)
 
 
 w = Worker()
